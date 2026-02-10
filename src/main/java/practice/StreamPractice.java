a0af8c3a955a491233a11ec0e3ce57f0d8111b04
Dima Babenko <klapster1@gmail.com>
2026-02-10 21:58:07 +0300

    n

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -21,8 +21,7 @@ public class StreamPractice {
                 .map(String::trim)
                 .mapToInt(Integer::parseInt).filter(n -> n % 2 == 0).min().orElseThrow(
                         () -> new RuntimeException(
-                                "Can't get min value from list: " + numbers
-                                        + " >"));
+                                "Can't get min value from list: " + numbers));
     }
 
     /**
